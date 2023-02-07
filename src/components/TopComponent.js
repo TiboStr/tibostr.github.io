1694c8b4e2a371f1aed2f6612c2c0d204c3cc5b0
Tibo Stroo <tibo.stroo@hotmail.be>
2023-02-07 12:12:49 +0100

    Init react app

---
+++ src/components/TopComponent.js
@@ -0,0 +1,12 @@
+function TopComponent() {
+
+    return <>
+        <div className={"TopComponent"}>
+            <div>Tibo Stroo</div>
+            <div>socials</div>
+            <div></div>
+        </div>
+    </>
+}
+
+export default TopComponent;
